0fe56d115c7900d0ebdddbff0f7fe2866f3f2147
edsctz <esc881@gmail.com>
2025-05-18 16:56:03 -0300

    add more neighborhoods

--- src/config/neighborhoods.ts
+++ src/config/neighborhoods.ts
@@ -61,6 +61,217 @@ export const neighborhoods: Record<string, Neighborhood> = {
       }
     }
   },
+    tambore3: {
+    id: 'tambore3',
+    name: 'Tamboré 3',
+    title: 'Casas a Venda e Locação no Tamboré 3',
+    subtitle: 'Imóveis selecionados e perfeitamente alinhados com o seu estilo de vida e preferências',
+    heroImage: 'https://pages.c21alpha.com.br/wp-content/uploads/2025/05/Tambore-3.webp',
+    showcaseProperty: {
+      image: 'https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu0REzepc+hFlr881To3glXCG3d61vNamnW2pxa3r5p7xheXjQNrM2LAn86rTTluC9p84YdF9GUV4S8khQkXoUZecPE9evTur-2PfYZRbZk1V2iQ1sxh553inWYj96F8S9xqqIK-Giye2R4bIX0JrLuBHR9o47l9MqBG1U1ByqR9Q9JG2ozrBZrT0PpW61WIYQklkpNCiTqzwi82OZ5htFvpqLHoC91cXWaoQmvqIQKNz8k8NqufBERmHlELVhPYgYMf+HIVXmqEGngvcH7VN6RzqlNWUgqSrPEyHMlNr9rmd+aC4aaz0TqH4cjjho5wN4ojKNo8Jddj3SDQVc3pnKDNWuUC5-6mZjE3ubXSK+lVHVhONg99q7HYkYAQDspFO7Seqxg==.jpg',
+      title: 'Mansão Luxuosa',
+      subtitle: 'Acabamentos de Altíssimo Padrão',
+      features: {
+        bedrooms: 5,
+        bathrooms: 6,
+        area: 1080,
+        mobiliado: true
+      }
+    },
+    budgetRanges: {
+      venda: {
+        min: 6000000,
+        max: 25000000,
+        step: 200000
+      },
+      locacao: {
+        min: 30000,
+        max: 60000,
+        step: 2000
+      }
+    }
+  },
+    residencial9: {
+    id: 'residencial9',
+    name: 'Alphaville Residencial 9',
+    title: 'Casas a Venda e Locação no Residencial 9',
+    subtitle: 'Imóveis selecionados e perfeitamente alinhados com o seu estilo de vida e preferências',
+    heroImage: 'https://pages.c21alpha.com.br/wp-content/uploads/2025/05/Residencial-9.webp',
+    showcaseProperty: {
+      image: 'https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu0lI8PJkxQodu+c8Y4WZhgveh7ySu4a9knawp7PRmJ+C25jOU5nU8d0l2LDKG1n1sol0ZcwHHWN3fssnSD-SB6ieB39qhiedj0zqEolLY3RW4SsLyBpc836OBMOO1XA-xBuiB6-YsiiqS7noWk1tB+VHR9o47l9MqBG1U1ByqR9Q9JG2ozrBZrT0PpW61WIYQklkpNCiTqzwi82OZ5htFvpqLHoC91cXWaoQmvqIQKNz8k8NqufBERmHlELVhPYgYMf+HIVXmqEGngvcH7VN6RzqlNWUgqSrPEyHMlNr9rmd+aC4aaz0TqH4cjjho5wN4ojKNo8Jddj3SDQVc3pnKDNWuUC5-6mZjE3ubXSK+lVHVhONg99q7HYkYAQDspFO7Seqxg==.jpg',
+      title: 'Excelente Oportunidade',
+      subtitle: 'Abaixo do Valor de Mercado',
+      features: {
+        bedrooms: 3,
+        bathrooms: 4,
+        area: 345,
+        mobiliado: false
+      }
+    },
+    budgetRanges: {
+      venda: {
+        min: 1500000,
+        max: 7000000,
+        step: 200000
+      },
+      locacao: {
+        min: 10000,
+        max: 30000,
+        step: 2000
+      }
+    }
+  },
+  casaavenda: {
+    id: 'casaavenda',
+    name: 'Alphaville',
+    title: 'Casas a Venda em Alphaville',
+    subtitle: 'Imóveis selecionados e perfeitamente alinhados com o seu estilo de vida e preferências',
+    heroImage: 'https://pages.c21alpha.com.br/wp-content/uploads/2025/05/Residencial-9.webp',
+    showcaseProperty: {
+      image: 'https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu0lI8PJkxQodu+c8Y4WZhgveh7ySu4a9knawp7PRmJ+C25jOU5nU8d0l2LDKG1n1sol0ZcwHHWN3fssnSD-SB6ieB39qhiedj0zqEolLY3RW4SsLyBpc836OBMOO1XA-xBuiB6-YsiiqS7noWk1tB+VHR9o47l9MqBG1U1ByqR9Q9JG2ozrBZrT0PpW61WIYQklkpNCiTqzwi82OZ5htFvpqLHoC91cXWaoQmvqIQKNz8k8NqufBERmHlELVhPYgYMf+HIVXmqEGngvcH7VN6RzqlNWUgqSrPEyHMlNr9rmd+aC4aaz0TqH4cjjho5wN4ojKNo8Jddj3SDQVc3pnKDNWuUC5-6mZjE3ubXSK+lVHVhONg99q7HYkYAQDspFO7Seqxg==.jpg',
+      title: 'Excelente Oportunidade',
+      subtitle: 'Abaixo do Valor de Mercado',
+      features: {
+        bedrooms: 3,
+        bathrooms: 4,
+        area: 345,
+        mobiliado: false
+      }
+    },
+    budgetRanges: {
+      venda: {
+        min: 1500000,
+        max: 10000000,
+        step: 200000
+      },
+      locacao: {
+        min: 10000,
+        max: 60000,
+        step: 5000
+      }
+    }
+  },
+    genesis: {
+    id: 'genesis12',
+    name: 'Genesis I e II',
+    title: 'Casas a Venda e Locação no Genesis 1 e 2',
+    subtitle: 'Imóveis selecionados e perfeitamente alinhados com o seu estilo de vida e preferências',
+    heroImage: 'https://pages.c21alpha.com.br/wp-content/uploads/2025/05/Genesis.webp',
+    showcaseProperty: {
+      image: 'https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu2d2+uxfuzUAkekHVYi+4jXa+dSK5fjAgW+ypsmG5auF+d7mTZzk6qMc8a7TClH0iKBDSfINCXdmSbJ4ZiXsdKauI2VTmmGOnBjMd-5gTmBb9T4NuSpNn0eOArLz10Q--iG1EsT0+haIXb3BB19DQPlLCdkx8QdQ8Bi1SUxy8h8d55m-oS7TO7S-K5+hxzlXFx5k9oa+Squ619jCLJU4Vr0lNm4L5VMZWe0S2-TJT-8+5wEDqv3JFhXchgzWgPAoc9j0QdhKmaoHiAvEGLFMu0zqlYLGg-atbkyGZVA897nN8afjOqj6Ef-xOUupjo0W5ISdfZoJdM-0XUdJQn52ZWBDplWm6tfGp02ncjSdth5HCl6OzpZqtXZrYEdR-9tU52I=.jpg',
+      title: 'Casa com Vista para a Mata',
+      subtitle: 'Genesis II - Alto Padrão',
+      features: {
+        bedrooms: 4,
+        bathrooms: 7,
+        area: 303,
+        mobiliado: true
+      }
+    },
+    budgetRanges: {
+      venda: {
+        min: 4000000,
+        max: 15000000,
+        step: 200000
+      },
+      locacao: {
+        min: 20000,
+        max: 50000,
+        step: 2000
+      }
+    }
+  },
+    residencial5: {
+    id: 'residencial5',
+    name: 'Alphaville Residencial 5',
+    title: 'Casas a Venda e Locação no Residencial 5',
+    subtitle: 'Imóveis selecionados e perfeitamente alinhados com o seu estilo de vida e preferências',
+    heroImage: 'https://pages.c21alpha.com.br/wp-content/uploads/2025/05/Residencial-5.webp',
+    showcaseProperty: {
+      image: 'https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtuxxB9c5ry0kCyulIB4+JrTS97InL7eahoUvP8pSMvYny0-T5feTu8Z8Ey5zXSgHxrrU-dvMVbhApRsh3ayXwcJ28Z0F-nzayoUCSHqMeb1gQ0wAUmxJglHajXq+L6Gx1xQa0BPPb7xehZa7WO1Z2ErZHR9o47l9MqBG1U1ByqR9Q9JG2ozrBZrT0PpW61WIYQklkpNCiTqzwi82OZ5htFvpqLHoC91cXWaoQmvqIQKNz8k8NqufBERmHlELVhPYgYMf+HIVXmqEGngvcH7VN6RzqlNWUgqSrPEyHMlNr9rmd+aC4aaz0TqH4cjjho5wN4ojKNo8Jddj3SDQVc3pnKDNWuUC5-6mZjE3ubXSK+lVHVhONg99q7HYkYAQDspFO7Seqxg==.jpg',
+      title: 'Casa Pronta para Morar',
+      subtitle: 'Para Venda ou Alugar',
+      features: {
+        bedrooms: 4,
+        bathrooms: 3,
+        area: 290,
+        mobiliado: false
+      }
+    },
+    budgetRanges: {
+      venda: {
+        min: 2000000,
+        max: 10000000,
+        step: 200000
+      },
+      locacao: {
+        min: 10000,
+        max: 30000,
+        step: 2000
+      }
+    }
+  },
+    burlemarx: {
+    id: 'burlemarx',
+    name: 'Burle Marx',
+    title: 'Casas a Venda e Locação no Burle Marx',
+    subtitle: 'Imóveis selecionados e perfeitamente alinhados com o seu estilo de vida e preferências',
+    heroImage: 'https://pages.c21alpha.com.br/wp-content/uploads/2025/05/Burle-Marx.webp',
+    showcaseProperty: {
+      image: 'https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu2x51fd+xDIutMcSUoemonSm8qq3zvyZ8Ri89q--uOjpx5vuV+mb5tsVifbKIFq2qLBpe-I1BHJ9TtFDdETJUbmLJT1tsze7phTSVpVMeHt-8RkYuisM9AHocseB3gU23hqlBdb+tSfXVr7tLH91QPlLCdkx8QdQ8Bi1SUxy8h8d55m-oS7TO7S-K5+hxzlXFx5k9oa+Squ619jCLJU4Vr0lNm4L5VMZWe0S2-TJT-8+5wEDqv3JFhXchgzWgPAoc9j0QdhKmaoHiAvEGLFMu0zqlYLGg-atbkyGZVA897nN8afjOqj6Ef-xOUupjo0W5ISdfZoJdM-0XUdJQn52ZWBDplWm6tfGp02ncjSdth5HCl6OzpZqtXZrYEdR-9tU52I=.jpg',
+      title: 'Casa Moderna',
+      subtitle: 'Burle Marx - Alto Padrão',
+      features: {
+        bedrooms: 3,
+        bathrooms: 4,
+        area: 230,
+        mobiliado: true
+      }
+    },
+    budgetRanges: {
+      venda: {
+        min: 2000000,
+        max: 15000000,
+        step: 200000
+      },
+      locacao: {
+        min: 15000,
+        max: 50000,
+        step: 2000
+      }
+    }
+  },
+  apartamento: {
+    id: 'apartamentoalugar',
+    name: 'Alphaville',
+    title: 'Apartamentos para Alugar em Alphaville',
+    subtitle: 'Conheça os melhores apartamentos disponíveis e disponíveis em Alphaville',
+    heroImage: 'https://pages.c21alpha.com.br/wp-content/uploads/2025/05/resorttambore.webp',
+    showcaseProperty: {
+      image: 'https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtux1a0Ylj2go-nuQzeLudiyyYxI+5woq4tBXTqZ7Onu3Zi8LNWefe4p817Y-VOHKh6IpBe9AHPk1+f7ZcHDbabt+LZiQI8yGOrX38YpdkeFF6miRroBQN7mTpB7Wc5noXxwS9VNPZ8QilFfv9Pl9TQPlLCdkx8QdQ8Bi1SUxy8h8d55m-oS7TO7S-K5+hxzlXFx5k9oa+Squ619jCLJU4Vr0lNm4L5VMZWe0S2-TJT-8+5wEDqv3JFhXchgzWgPAoc9j0QdhKmaoHiAvEGLFMu0zqlYLGg-atbkyGZVA897nN8afjOqj6Ef-xOUupjo0W5ISdfZoJdM-0XUdJQn52ZWBDplWm6tfGp02ncjSdth5HCl6OzpZqtXZrYEdR-9tU52I=.jpg',
+      title: 'Apartamento com 3 Quartos',
+      subtitle: 'A Venda ou Alugar',
+      features: {
+        bedrooms: 3,
+        bathrooms: 3,
+        area: 133,
+        mobiliado: true
+      }
+    },
+    budgetRanges: {
+      venda: {
+        min: 1000000,
+        max: 1700000,
+        step: 100000
+      },
+      locacao: {
+        min: 5000,
+        max: 20000,
+        step: 1000
+      }
+    }
+  }
+},
   resorttambore: {
     id: 'resorttambore',
     name: 'Resort Tamboré',
